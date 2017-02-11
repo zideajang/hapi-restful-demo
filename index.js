62a642f1f8cacf2be66b9526d59162bbba18f273
zideajang <zidea2015@163.com>
2017-02-11 15:37:52 +0800

    创建数据

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 //连接服务器
 server.connection({ port: 8000, host: 'localhost' });
+const tuts = 
+
+  [
+    {
+      title: "nodejs basic tutorial",
+      summary: "this is tutorial of basic nodejs"
+    },
+    {
+      title: "nodejs basic tutorial1",
+      summary: "this is tutorial1 of basic nodejs"
+    },
+    {
+      title: "nodejs basic tutorial2",
+      summary: "this is tutorial2 of basic nodejs"
+    }
+  ];
 
 server.route({
   method: 'GET',
   path: '/tuts',
   handler: function(request, reply) {
-    reply({ 'data' : '' });
+    reply({ 'data' :  tuts });
   }
 });
 
